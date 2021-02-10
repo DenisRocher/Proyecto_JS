691893fc70abeba785729c3f690a78b860db1605
Denis Rocher <denis.rocher@gmail.com>
2021-02-10 09:14:20 -0300

    Task 2: Plugin jQuery Slider

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
 'use strict'
+
+$(function(){
+
+  $('.galeria').bxSlider({
+    auto: true,
+    mode: 'fade',
+    slideWidth: 600,
+    autoControls: true,
+    stopAutoOnClick: true,
+    pager: true,
+    slideWidth: 1200,
+    pager: false
+  });
+
+});
